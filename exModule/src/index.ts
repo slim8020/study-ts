138fffa34cae5ccc5146696269f337f6a0f89e26
noleunja <noleunjasoft@gmail.com>
2022-02-13 22:04:11 +0900

    Modified exModules/index.ts : Variable Error

--- exModule/src/index.ts
+++ exModule/src/index.ts
@@ -4,10 +4,7 @@ const testMakePerson = (): void => {
     let jane: IPerson = makePerson('Jane');
     let jack: IPerson = makePerson('Jack');
 
-    n = "abc";
-
     console.log(jane, jack);
-    console.log(n);
 }
 
 testMakePerson();
