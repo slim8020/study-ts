ac390b7a14cba6d185feb58c669e4a2f9d3f70c9
noleunja_mac <noleunjasoft@gmail.com>
2022-02-23 19:09:39 +0900

    Add exArray

--- exArray/src/index.ts
+++ exArray/src/index.ts
@@ -1,3 +1,59 @@
 import * as ex from './utils/exArrary'
 
 ex.print();
+
+// 배열 쪼개기
+console.log(ex.split('hello'));
+console.log(ex.split('나는 알고 싶다. 무엇이든. 하지만 알 수가 없다.', '.'));
+// 배열 합치기
+console.log(ex.join(['h','e','l','l','o']));
+console.log(ex.join(['stay hungry','stay foolish'],','));
+console.log(ex.getNumItem([1,10,43,23,22],2));
+
+// index
+let array: number[] = [3,21,453,23,1];
+let [one,two,three, ...rest] = array;
+console.log(one, two, three, rest);
+
+// for.. in
+let fruits = ['Apple', 'Orange', 'Melon'];
+for(let i in fruits){
+    console.log(`${i}: ${fruits[i]}`);
+}
+
+let mj:any = {name: 'mj', age: 43};
+for(let prop in mj){
+    console.log(`${prop}:${mj[prop]}`);
+}
+
+console.log(Object.keys(mj));
+console.log(Object.values(mj));
+
+for(let name of fruits){
+    console.log(name);
+}
+
+let numArr:number[] = [1,2,3];
+let strArr:string[] = ['Hello', 'World'];
+
+type IPerson = {name:string, age?:number};
+let personArr:IPerson[] = [{name: 'mj'}, {name:'mj', age:32}];
+
+console.log( ex.arrayLength(numArr), 
+ex.arrayLength(strArr), 
+ex.arrayLength(personArr), 
+ex.isEmpty([]),
+ex.isEmpty([1]));
+
+const identity = <T>(n:T):T => n;
+console.log(identity<boolean>(true), identity<number>(3), identity(5));
+
+console.log(ex.range(25, 30+7));
+
+let numbers: number[] = ex.range(1, 7);
+let result = ex.fold(numbers, (result, value) => result + value, 0);
+console.log(result);
+
+
+
+
--- exArray/src/utils/exArrary.ts
+++ exArray/src/utils/exArrary.ts
@@ -1,3 +1,38 @@
 export function print(){
-    console.log("Hello");
+    let numbers = [ 1,2,3 ];
+    let strings = ['Hello', 'World'];   //Array Class 의 인스턴스
+    let obj = {name: 'Jack', age: 32};
+    
+    let numArray: number[] = [1,2,3];
+    let strArray: string[] = ['Hello','World'];
+
+    type IPerson = {name: string, age?: number};
+    let personArray:IPerson[] = [{name:'Jack'}, {name: 'Jane', age:32}];
+
+
+    console.log(Array.isArray(numbers), Array.isArray(strings), Array.isArray(obj));
+    console.log(numArray, strArray, personArray);
+    console.log(personArray[0].name);
+}
+
+export const split = (str:string, delim:string = ''):string[] => str.split(delim);
+
+export const join = (strArray: string[], delim: string=''):string =>strArray.join(delim);
+
+export const getNumItem = (numArray: number[], index: number):number => {return numArray[index];}
+
+export const arrayLength = <T>(array:T[]):number => array.length;
+
+export const isEmpty = <T>(array:T[]):boolean => arrayLength<T>(array) === 0;
+
+export const range = (from: number, to:number):number[] =>
+    from < to ? [from, ...range(from+1,to)] : [];
+
+export const fold = <T>(array: T[], callback: (result: T, val: T) => T, initValue:T) =>{
+    let result: T = initValue;
+    for(let i=0; i< array.length; ++i){
+        const value = array[i];
+        result = callback(result, value);
+    }
+    return result;
 }
